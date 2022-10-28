5e42d50a910a2255e26095aa6b1a14dfbe74c166
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-10-28 19:42:18 +0300

    added comments

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let divTemasNode = document.querySelector(".teams-generated")
 let numberValueNode = document.getElementById("number-value")
 
 let names = []
-let numberOfTeams = 0
+let numberOfTeams = 1
 const decrementTeams = () => {
-  if (numberOfTeams > 0) {
+  if (numberOfTeams > 1) {
     numberOfTeams--
     numberValueNode.innerText = numberOfTeams
   }
@@ -20,9 +20,10 @@ const incrementTeams = () => {
 
 const addName = () => {
   let nameValue = textInputNode.value
+  if (nameValue.length === 0) {
+    return
+  }
   names.push(nameValue)
-
-  console.log(names)
   liNode = document.createElement("li")
   nameUlNode.appendChild(liNode)
   liNode.innerText = nameValue
@@ -35,27 +36,34 @@ const resetTeams = () => {
 }
 
 let generateTeams = () => {
-  let ulNodes = []
-
+  let teamNodes = []
   divTemasNode.innerHTML = ""
   for (let i = 0; i < numberOfTeams; i++) {
     let h4Node = document.createElement("h4")
     let teamContainerNode = document.createElement("div")
+    teamContainerNode.classList.add("card")
     h4Node.innerHTML = `Team ${i + 1}`
     teamContainerNode.appendChild(h4Node)
     divTemasNode.appendChild(teamContainerNode)
     let ulNode = document.createElement("ul")
-    ulNode.classList.add("team")
-    ulNodes.push(ulNode)
+    teamContainerNode.classList.add("team")
+    teamNodes.push(ulNode)
     teamContainerNode.appendChild(ulNode)
   }
+  // store the number of names because when we remove a name the length will change
   let nameLength = names.length
   for (let i = 0; i < nameLength; i++) {
+    // choose a random index
     let randomIndex = Math.floor(Math.random() * names.length)
-
     let liNode = document.createElement("li")
     liNode.innerText = names[randomIndex]
-    ulNodes[i % numberOfTeams].appendChild(liNode)
+    // choose teams one after the other
+    // example with 3 teams
+    // i          = 0 1 2 3 4 5 6 7 8 9
+    // teamIndex  = 0 1 2 0 1 2 0 1 2 0
+    let teamIndex = i % numberOfTeams
+    teamNodes[teamIndex].appendChild(liNode)
+    // remove the selected name from the array
     names.splice(randomIndex, 1)
   }
 }
