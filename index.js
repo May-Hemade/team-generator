19a65c9698184be75e543313cb0e09a40d6c3102
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-10-28 19:09:28 +0300

    remove some unneeded code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let textInputNode = document.getElementById("name-input")
-let numberInputNode = document.getElementById("number-input")
 let nameFieldNode = document.querySelector(".name-grid")
 let nameUlNode = document.querySelector(".names-list")
-let listUlNode = document.querySelectorAll(".name-list")
 let divTemasNode = document.querySelector(".teams-generated")
 let numberValueNode = document.getElementById("number-value")
 
@@ -38,7 +36,6 @@ const resetTeams = () => {
 
 let generateTeams = () => {
   let ulNodes = []
-  //   let randomNames = []
 
   divTemasNode.innerHTML = ""
   for (let i = 0; i < numberOfTeams; i++) {
@@ -56,19 +53,11 @@ let generateTeams = () => {
   for (let i = 0; i < nameLength; i++) {
     let randomIndex = Math.floor(Math.random() * names.length)
 
-    // randomNames.push(names[randomIndex])
-
     let liNode = document.createElement("li")
     liNode.innerText = names[randomIndex]
     ulNodes[i % numberOfTeams].appendChild(liNode)
     names.splice(randomIndex, 1)
   }
-  //   for (let i = 0; i < randomNames.length; i++) {
-  //     const name = randomNames[i]
-  //     let liNode = document.createElement("li")
-  //     liNode.innerText = name
-  //     ulNodes[i % numberOfTeams].appendChild(liNode)
-  //   }
 }
 
 window.onload = () => {
